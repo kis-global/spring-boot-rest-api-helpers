ba8c023b874d661dfd8bb4390f90460be9f10293
CaMac <camt1@fpt.com>
2024-12-24 09:34:27 +0700

    Testing

--- src/main/java/springboot/rest/specifications/CustomSpecifications.java
+++ src/main/java/springboot/rest/specifications/CustomSpecifications.java
@@ -15,8 +15,13 @@ import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.Attribute;
 import javax.persistence.metamodel.IdentifiableType;
 import javax.persistence.metamodel.Metamodel;
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -29,10 +34,15 @@ import java.util.UUID;
 @Service
 public class CustomSpecifications<T> {
 
+    private static final SimpleDateFormat FORMATTER_DT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat FORMATTER_D = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateTimeFormatter FORMATTER_LD = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private static final DateTimeFormatter FORMATTER_LDT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @PersistenceContext
     private EntityManager em;
 
-    public Specification<T> customSpecificationBuilder(Map<String,Object> map) {
+    public Specification<T> customSpecificationBuilder(Map<String, Object> map) {
         return (Specification<T>) (root, query, builder) -> {
             query.distinct(true);
             List<Predicate> predicates = handleMap(builder, root, null, query, map, new ArrayList<>());
@@ -40,33 +50,33 @@ public class CustomSpecifications<T> {
         };
     }
 
-    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, Map<String,Object> map) {
+    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, Map<String, Object> map) {
         query.distinct(true);
         List<Predicate> predicates = handleMap(builder, root, null, query, map, new ArrayList<>());
         return builder.and(predicates.toArray(new Predicate[predicates.size()]));
     }
 
-    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, Map<String,Object> map, List<String> includeOnlyFields) {
+    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, Map<String, Object> map, List<String> includeOnlyFields) {
         query.distinct(true);
         List<Predicate> predicates = handleMap(builder, root, null, query, map, includeOnlyFields);
         return builder.and(predicates.toArray(new Predicate[predicates.size()]));
     }
 
-    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, List<Map<String,Object>> list) {
+    public Predicate customSpecificationBuilder(CriteriaBuilder builder, CriteriaQuery query, Root root, List<Map<String, Object>> list) {
         query.distinct(true);
         List<Predicate> orPredicates = new ArrayList<>();
-        for (Map<String, Object> map: list) {
+        for (Map<String, Object> map : list) {
             List<Predicate> predicates = handleMap(builder, root, null, query, map, new ArrayList<>());
-            Predicate orPred =  builder.and(predicates.toArray(new Predicate[predicates.size()]));
+            Predicate orPred = builder.and(predicates.toArray(new Predicate[predicates.size()]));
             orPredicates.add(orPred);
         }
         return builder.or(orPredicates.toArray(new Predicate[orPredicates.size()]));
     }
 
     public List<Predicate> handleMap(CriteriaBuilder builder, Root root, Join join, CriteriaQuery query,
-        Map<String,Object> map, List<String> includeOnlyFields) {
+                                     Map<String, Object> map, List<String> includeOnlyFields) {
 
-        if (join != null){
+        if (join != null) {
             root = query.from(getJavaTypeOfClassContainingAttribute(root, join.getAttribute().getName()));
         }
         List<Predicate> predicates = new ArrayList<>();
@@ -75,7 +85,7 @@ public class CustomSpecifications<T> {
             predicates.add(searchInAllAttributesPredicate(builder, root, (String) map.get("q"), includeOnlyFields));
             map.remove("q");
         }
-        Set<Attribute<? super T,?>> attributes = root.getModel().getAttributes();
+        Set<Attribute<? super T, ?>> attributes = root.getModel().getAttributes();
         for (Map.Entry e : map.entrySet()) {
             String key = (String) e.getKey();
             Object val = e.getValue();
@@ -91,7 +101,7 @@ public class CustomSpecifications<T> {
     }
 
     public Predicate handleAllCases(CriteriaBuilder builder, Root root, Join join, CriteriaQuery query,
-        Attribute a, String key, Object val) {
+                                    Attribute a, String key, Object val) {
 
         boolean isValueCollection = val instanceof Collection;
         boolean isValueMap = val instanceof Map;
@@ -109,7 +119,7 @@ public class CustomSpecifications<T> {
             val = convertMapContainingPrimaryIdToValue(val, a, root);
         }
         if (val instanceof Map && isAssociation) {
-            List<Predicate> predicates = handleMap(builder, root, addJoinIfNotExists(root,a, isValueCollection, isConjunction), query, ((Map) val), Arrays.asList());
+            List<Predicate> predicates = handleMap(builder, root, addJoinIfNotExists(root, a, isValueCollection, isConjunction), query, ((Map) val), Arrays.asList());
             Predicate[] predicatesArray = predicates.toArray(new Predicate[predicates.size()]);
             return builder.and(predicatesArray);
         }
@@ -123,19 +133,19 @@ public class CustomSpecifications<T> {
                 return handleCollection(builder, root, join, query, a, cleanKey, (Collection) val, true);
             }
         } else if (isLte) {
-            return createLtePredicate(builder, root, a, val);
+            return invokePredicate(builder, root, a, val, "lessThanOrEqualTo");
         } else if (isGte) {
-            return createGtePredicate(builder, root, a, val);
+            return invokePredicate(builder, root, a, val, "greaterThanOrEqualTo");
         } else if (isLt) {
-            return createLtPredicate(builder, root, a, val);
+            return invokePredicate(builder, root, a, val, "lessThan");
         } else if (isGt) {
-            return createGtPredicate(builder, root, a, val);
+            return invokePredicate(builder, root, a, val, "greaterThan");
         }
         return builder.conjunction();
     }
 
     public Predicate handleCollection(CriteriaBuilder builder, Root root, Join join, CriteriaQuery query,
-        Attribute a, String key, Collection values, boolean conjunction) {
+                                      Attribute a, String key, Collection values, boolean conjunction) {
 
         List<Predicate> predicates = new ArrayList<>();
         for (Object val : values) {
@@ -147,7 +157,7 @@ public class CustomSpecifications<T> {
     }
 
     public Predicate handleCleanKeyCase(CriteriaBuilder builder, Root root, Join join, CriteriaQuery query,
-        String key, Attribute a, Object val) {
+                                        String key, Attribute a, Object val) {
 
         boolean isValueCollection = val instanceof Collection;
         boolean isValTextSearch = (val instanceof String) && ((String) val).contains("%");
@@ -181,7 +191,7 @@ public class CustomSpecifications<T> {
     }
 
     public Predicate searchInAllAttributesPredicate(CriteriaBuilder builder, Root root, String text,
-        List<String> includeOnlyFields) {
+                                                    List<String> includeOnlyFields) {
 
         if (!text.contains("%")) {
             text = "%" + text + "%";
@@ -205,50 +215,47 @@ public class CustomSpecifications<T> {
     /**
      * creates an equality predicate.
      * Basically it checks for type and does the right predicate.
+     *
      * @param builder CriteriaBuilder
-     * @param root Root
-     * @param join Join Context
-     * @param a Attribute
-     * @param val Object for comparison (from user)
+     * @param root    Root
+     * @param join    Join Context
+     * @param a       Attribute
+     * @param val     Object for comparison (from user)
      * @return Predicate
      */
     private Predicate createEqualityPredicate(CriteriaBuilder builder, Root root, Join join, Attribute a, Object val) {
         if (isNull(a, val)) {
             if (a.isAssociation() && a.isCollection()) {
                 return builder.isEmpty(root.get(a.getName()));
-            }
-            else if(isPrimitive(a)) {
+            } else if (isPrimitive(a)) {
                 return builder.isNull(root.get(a.getName()));
-            }
-            else {
+            } else {
                 return root.get(a.getName()).isNull();
             }
-        }
-        else if (join == null) {
+        } else if (join == null) {
             if (isEnum(a)) {
                 return builder.equal(root.get(a.getName()), Enum.valueOf(Class.class.cast(a.getJavaType()), (String) val));
             } else if (isPrimitive(a)) {
                 return builder.equal(root.get(a.getName()), val);
-            } else if(isUUID(a)) {
+            } else if (isUUID(a)) {
                 return builder.equal(root.get(a.getName()), UUID.fromString(val.toString()));
-            } else if(a.isAssociation()) {
+            } else if (a.isAssociation()) {
                 if (isPrimaryKeyOfAttributeUUID(a, root)) {
                     return prepareJoinAssociatedPredicate(builder, root, a, UUID.fromString(val.toString()));
-                }
-                else {
+                } else {
                     return prepareJoinAssociatedPredicate(builder, root, a, val);
                 }
+            } else {
+                return invokePredicate(builder, root, a, val, null);
             }
-        }
-        else if (join != null) {
+        } else if (join != null) {
             if (isEnum(a)) {
                 return builder.equal(join.get(a.getName()), Enum.valueOf(Class.class.cast(a.getJavaType()), (String) val));
             } else if (isPrimitive(a)) {
                 return builder.equal(join.get(a.getName()), val);
             } else if (a.isAssociation()) {
                 return builder.equal(join.get(a.getName()), val);
-            }
-            else if(a.isCollection()) {
+            } else if (a.isCollection()) {
                 return builder.equal(join, val);
             }
         }
@@ -263,60 +270,44 @@ public class CustomSpecifications<T> {
         }
     }
 
-    private Predicate createGtPredicate(CriteriaBuilder builder, Root root, Attribute a, Object val) {
-        if (val instanceof String) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try {
-                return builder.greaterThan(root.get(a.getName()), formatter.parse(((String) val)));
-            } catch (ParseException ignored) {
+    private Predicate invokePredicate(CriteriaBuilder builder, Root root, Attribute a, Object val, String methodName) {
+        try {
+            if (LocalDate.class.equals(a.getJavaType())) {
+                return invokePredicateMethod(methodName, builder, root, a, LocalDate.parse((String) val, FORMATTER_LD));
+            } else if (Date.class.equals(a.getJavaType())) {
+                return invokePredicateMethod(methodName, builder, root, a, FORMATTER_DT.parse(((String) val)));
+            } else if (LocalDateTime.class.equals(a.getJavaType())) {
+                return invokePredicateMethod(methodName, builder, root, a, LocalDateTime.parse((String) val, FORMATTER_LDT));
+            } else if (Timestamp.class.equals(a.getJavaType())) {
+                return invokePredicateMethod(methodName, builder, root, a, new Timestamp((Long) val));
+            } else if (String.class.equals(a.getJavaType())) {
+                return invokePredicateMethod(methodName, builder, root, a, (String) val);
+            } else if (val instanceof Integer) {
+                invokePredicateMethod(methodName, builder, root, a, (Integer) val);
             }
-            return builder.greaterThan(root.get(a.getName()), ((String) val));
-        } else if (val instanceof Integer) {
-            return builder.greaterThan(root.get(a.getName()), (Integer) val);
+            throw new IllegalArgumentException("val type not supported yet for lower-equals");
+        } catch (DateTimeParseException | ParseException e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
-        throw new IllegalArgumentException("val type not supported yet for greater");
     }
 
-    private Predicate createGtePredicate(CriteriaBuilder builder, Root root, Attribute a, Object val) {
-        if (val instanceof String) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try {
-                return builder.greaterThanOrEqualTo(root.get(a.getName()), formatter.parse(((String) val)));
-            } catch (ParseException ignored) {
-            }
-            return builder.greaterThanOrEqualTo(builder.lower(root.get(a.getName())), ((String) val).toLowerCase());
-        } else if (val instanceof Integer) {
-            return builder.greaterThanOrEqualTo(root.get(a.getName()), (Integer) val);
+    private <Y extends Comparable<? super Y>> Predicate invokePredicateMethod(String methodName,
+                                                                              CriteriaBuilder builder,
+                                                                              Root root,
+                                                                              Attribute a,
+                                                                              Y value) {
+        if ("greaterThanOrEqualTo".equals(methodName)) {
+            return builder.greaterThanOrEqualTo(root.get(a.getName()), value);
+        } else if ("lessThan".equals(methodName)) {
+            return builder.lessThan(root.get(a.getName()), value);
+        } else if ("lessThanOrEqualTo".equals(methodName)) {
+            return builder.lessThanOrEqualTo(root.get(a.getName()), value);
+        } else if ("greaterThan".equals(methodName)) {
+            return builder.greaterThan(root.get(a.getName()), value);
+        } else if (StringUtils.isEmpty(methodName)) {
+            return builder.equal(root.get(a.getName()), value);
         }
-        throw new IllegalArgumentException("val type not supported yet for greater-equals");
-    }
-
-    private Predicate createLtPredicate(CriteriaBuilder builder, Root root, Attribute a, Object val) {
-        if (val instanceof String) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try {
-                return builder.lessThan(root.get(a.getName()), formatter.parse(((String) val)));
-            } catch (ParseException ignored) {
-            }
-            return builder.lessThan(builder.lower(root.get(a.getName())), ((String) val).toLowerCase());
-        } else if (val instanceof Integer) {
-            return builder.lessThan(root.get(a.getName()), (Integer) val);
-        }
-        throw new IllegalArgumentException("val type not supported yet for lower");
-    }
-
-    private Predicate createLtePredicate(CriteriaBuilder builder, Root root, Attribute a, Object val) {
-        if (val instanceof String) {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try {
-                return builder.lessThanOrEqualTo(root.get(a.getName()), formatter.parse(((String) val)));
-            } catch (ParseException ignored) {
-            }
-            return builder.lessThanOrEqualTo(builder.lower(root.get(a.getName())), ((String) val).toLowerCase());
-        } else if (val instanceof Integer) {
-            return builder.lessThanOrEqualTo(root.get(a.getName()), (Integer) val);
-        }
-        throw new IllegalArgumentException("val type not supported yet for lower-equals");
+        throw new IllegalArgumentException();
     }
 
     private Predicate prepareJoinAssociatedPredicate(CriteriaBuilder builder, Root root, Attribute a, Object val) {
@@ -333,9 +324,9 @@ public class CustomSpecifications<T> {
 
         Set<Join> joins = root.getJoins();
         Join toReturn = null;
-        for (Join join: joins) {
-            if (a.getName().equals(join.getAttribute().getName())){
-                toReturn =  join;
+        for (Join join : joins) {
+            if (a.getName().equals(join.getAttribute().getName())) {
+                toReturn = join;
                 break;
             }
         }
@@ -364,7 +355,7 @@ public class CustomSpecifications<T> {
         String primaryKeyName = getIdAttribute(em, javaTypeOfAttribute).getName();
         if (val instanceof Map && ((Map) val).keySet().size() == 1) {
             Map map = ((Map) val);
-            for (Object key: map.keySet()) {
+            for (Object key : map.keySet()) {
                 if (key.equals(primaryKeyName)) {
                     return map.get(primaryKeyName);
                 }
@@ -388,7 +379,7 @@ public class CustomSpecifications<T> {
                 attributeJavaClass.equals("double");
     }
 
-   private boolean isPrimitiveValue(Object obj) {
+    private boolean isPrimitiveValue(Object obj) {
         String javaClass = obj.getClass().getSimpleName().toLowerCase();
         return javaClass.startsWith("int") ||
                 javaClass.startsWith("long") ||
